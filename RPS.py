12346299dcac490ca48c281e419f6390c179bf9b
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-06-12 17:42:33 -0500

    added notes from first few minutes

---
+++ RPS.py
@@ -0,0 +1,22 @@
+# Functions declared with def
+# Function scope defined by indentation
+def get_choices():
+  player_choice = 'rock'
+  computer_choice = 'paper'
+  
+  return computer_choice
+
+
+11:18
+
+
+
+
+
+# def greeting():
+#   return 'Hi'
+
+# Can assign variable to function return value like in JS
+# response = greeting()
+
+# print(response)
