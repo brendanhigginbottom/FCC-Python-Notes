63a8111779b17bb5269dde6996fcade0fd968ac1
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-07-28 18:18:18 -0500

    more basics, taking notes

---
+++ Notes.py
@@ -0,0 +1,33 @@
+##  Covering Basic Commands of Python
+
+##! Variables
+# assignment uses = 
+# variables cannot start with a number, can't use ! or %. Also can't use keywords
+name = 'Brendan'
+age = 30
+
+ ##! Expressions return values
+ # 1+1 or 'Brendan'
+ ## Statement is operation on a value
+ ## A program is a series of statements
+ # Can put multiple statements on one line separated by ; i.e., name = 'Tom'; print(name)
+
+# Scope is defined by indentation
+
+##! Data types
+# Can check type with type(), i.e. print(type(name)) or isinstance, i.e.
+# print(isinstance(name, str))
+# String 'Brendan'
+# Numbers - int and float print(isintance(variable, [int][float]))
+# complex
+# bool - boolean
+# tuple - tuples
+# range - ranges
+# dict - dictionaries (objects(?))
+# set - sets
+
+# Can force type or cast/casting, i.e. age = float(2), name = int('20')
+
+##! Operators
+# = is assignment
+# arithmatic operators +, -, *, /, %, ** (power), // (floor division rounds down)
