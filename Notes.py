767e78cd11f602f082578001ea08fa56369898e3
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-07-29 13:06:52 -0500

    more data types, string methods, definitions of all operators

--- Notes.py
+++ Notes.py
@@ -26,8 +26,87 @@ age = 30
 # dict - dictionaries (objects(?))
 # set - sets
 
-# Can force type or cast/casting, i.e. age = float(2), name = int('20')
+# Can force type or cast/casting, i.e. age = float(2), age = int('20')
 
 ##! Operators
 # = is assignment
 # arithmatic operators +, -, *, /, %, ** (power), // (floor division rounds down)
+# boolean - same as JS True/False
+# boolean operators not, and, or
+# i.e, condition1 = True
+#      condition2 = False
+#      not condition1 // False
+#      condiiton1 and condition2 // False
+#      condition1 or condition2 // True
+    ##! Specific to or - returns first non falsy value
+    ##! and returns last operand if all are falsy (last two examples below)
+    ##! or put another way: if x == False, then y, else x
+        #print(0 or 1) // 1
+        #print(False or 'hey) // 'hey'
+        #print('hi' or 'hey) // 'hi'
+        #print([] or False) // False
+        #print(False or []) // []
+    ##! Specific to and - only evaluates second if first is true
+    ##! if first is falsy (0, False, empty data types), returns that argument
+    ##! and otherwise evaluates second
+    ##! If x = false, then x, else y
+        #print(0 and 1) // 0
+        #print(1 and 0) // 0
+        #print (False and 'hey') // False
+        #print('hi' and 'hey') // 'hey'
+        #print([] and False) // []
+        #print(False and []) // False
+
+##! bitwise operators - rarely used but worth knowing
+    # & performs binary AND
+    # | performs binary OR
+    # ^ performs a binary XOR operation
+    # ~ performs a binary NOT operation
+    # << shift left operation
+    # >> shift right operation
+
+##! is - identity operator; used to compare two objects, returns True if both are same object
+##! in - membershop operator; used to tell if a value is contained in a list or another sequence
+
+##! ternary operator - write out if/else instead of condition ? truthy : falsy in JS
+# def is_adult(age):
+#    return True if age > 18 else False
+
+##! Strings
+# Can use both " " and ' ' 
+# Can concatanate with + operator
+# Can be multi-line if enclosed in three " or '
+    # i.e, print("""Brendan is
+    # 
+    # 30
+    # 
+    #years old
+    # """) //Brendan is
+    #
+    #       30
+    #
+    #       years old
+    ##! Built in methods
+    # print('Brendan'.upper()) // BRENDAN
+    # print('BrenDan'.lower()) // brendan
+    # print('bREndan person'.title) // Brendan Person - makes first letter capital
+    # .islower() // checks if all letters are all lower
+    ##! All the methods don't mutate and return new string
+    ##! len() is a global function (works with multiple type of data)
+    ##! and returns length
+    # name = 'Brendan'
+    # print(len(name)) // 7
+    # print('br' in name.lower()) // True - checks substring and is case sensitive
+    ##! Escaping quotes with \ -- really only needed if string needs ' and "
+    # print("Bre\"ndan") // Bre"ndan 
+    ##! \n = new line
+    # print('Bre\nndan') // Bre
+    #                       ndan
+    ##! getting letter by index, can start at -1 to begin at last letter
+    # print(name[0]) // B
+    # print(name[-1]) // n
+        ##! getting range with slicing - 
+        ##! for x:y, start at index x up to (not including) index y
+            # print(name[1:3]) // re
+
+# 1:21:16
